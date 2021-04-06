4509beb990dc7a04428996be5855bfdc2194c4c9
LuisCor07 <luisen.cortes.07@gmail.com>
2021-04-05 22:10:58 -0500

    First Commit

---
+++ Debug/source/sys_main.d
@@ -0,0 +1,93 @@
+# FIXED
+
+source/sys_main.obj: ../source/sys_main.c
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/FreeRTOS.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/FreeRTOSConfig.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_projdefs.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_portable.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_portmacro.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_mpu_wrappers.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_task.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_list.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/sys_core.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/sys_common.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/hal_stdtypes.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/gio.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_gio.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/sci.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_sci.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/KaanSat_Lib/Utilities.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/het.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_het.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/string.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/adc.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_adc.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/math.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_defs.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_limits.h
+source/sys_main.obj: C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/strings.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/KaanSat_Lib/Commands.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/KaanSat_Lib/PWM.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/esm.h
+source/sys_main.obj: C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_esm.h
+
+../source/sys_main.c: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/FreeRTOS.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stddef.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_ti_config.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/linkage.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_stdint40.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_types.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/sys/_stdint.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/FreeRTOSConfig.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_projdefs.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_portable.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_portmacro.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_mpu_wrappers.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_task.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/os_list.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/sys_core.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/sys_common.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/hal_stdtypes.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdbool.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/gio.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_gio.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/sci.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_sci.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/KaanSat_Lib/Utilities.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/het.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_het.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/string.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/adc.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_adc.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdio.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdarg.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/stdlib.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/math.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/_defs.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/machine/_limits.h: 
+C:/ti/ccs930/ccs/tools/compiler/ti-cgt-arm_18.12.4.LTS/include/strings.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/KaanSat_Lib/Commands.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/KaanSat_Lib/PWM.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/esm.h: 
+C:/Users/ADMIN/workspace_v9/CC2021_RM42x/include/reg_esm.h: 
---
+++ source/sys_main.c
@@ -0,0 +1,229 @@
+/** @file sys_main.c 
+*   @brief Application main file
+*   @date 11-Dec-2018
+*   @version 04.07.01
+*
+*   This file contains an empty main function,
+*   which can be used for the application.
+*/
+
+/* 
+* Copyright (C) 2009-2018 Texas Instruments Incorporated - www.ti.com 
+* 
+* 
+*  Redistribution and use in source and binary forms, with or without 
+*  modification, are permitted provided that the following conditions 
+*  are met:
+*
+*    Redistributions of source code must retain the above copyright 
+*    notice, this list of conditions and the following disclaimer.
+*
+*    Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the 
+*    documentation and/or other materials provided with the   
+*    distribution.
+*
+*    Neither the name of Texas Instruments Incorporated nor the names of
+*    its contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*
+*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+*  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+*  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+*  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+*  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+*  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+*  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+*  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+*  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*/
+
+
+/* USER CODE BEGIN (0) */
+/*----------------- OS Libraries --------------------*/
+#include "FreeRTOS.h"
+#include "os_task.h"
+#include "sys_core.h"
+#include "gio.h"
+#include "sci.h"
+/*------------- KA'AN SAT Libraries -----------------*/
+#include "KaanSat_Lib/Utilities.h"
+#include "KaanSat_Lib/Commands.h"
+#include "KaanSat_Lib/PWM.h"
+/* USER CODE END */
+
+/* Include Files */
+
+#include "sys_common.h"
+
+/* USER CODE BEGIN (1) */
+static char receivedData[20];
+int i = 0;
+/* USER CODE END */
+
+/** @fn void main(void)
+*   @brief Application main function
+*   @note This function is empty by default.
+*
+*   This function is called after startup.
+*   The user can use this function to implement the application.
+*/
+
+/* USER CODE BEGIN (2) */
+void sciNotification(sciBASE_t *sci, unsigned flags);
+/* USER CODE END */
+
+int main(void)
+{
+/* USER CODE BEGIN (3) */
+
+    gioInit();
+    hetInit();
+    adcInit();
+
+    /* ---------------- SCI CONFIG----------------*/
+    sciInit();
+    sciEnableNotification(scilinREG, SCI_RX_INT);
+    _enable_IRQ();
+    _enable_interrupt_();
+    sciReceive(scilinREG, 1, ( unsigned char *)receivedData);
+    /* --------------- READ SD STATE --------------*/
+
+    /* --------------- CREATE TASK --------------*/
+    xTaskCreate(vMissionOperations,"Sat Ops",configMINIMAL_STACK_SIZE, NULL, 1, &xWTStartHandle);
+    xTaskCreate(vSensors,"Sensores",configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+    xTaskCreate(vTelemetry,"T. Container",512, NULL, 1, &xTelemetryHandle);
+    xTaskCreate(vWaitToStart,"W.To S.",configMINIMAL_STACK_SIZE, NULL, 1, &xWTStartHandle);
+
+    vTaskSuspend(xTelemetryHandle);
+    vTaskStartScheduler();
+    while(1);
+/* USER CODE END */
+
+    return 0;
+}
+
+
+/* USER CODE BEGIN (4) */
+void vWaitToStart(void *pvParameters)
+{
+    portTickType xWaitTime;
+    xWaitTime = xTaskGetTickCount();
+
+    while(1)
+    {
+        if(telemetry_ON)
+        {
+            __delay_cycles(106);
+            vTaskResume(xTelemetryHandle);
+            vTaskSuspend(NULL);
+            __delay_cycles(106);
+        }
+        sciSendData(sprintf(command, "."), command, 0);
+        vTaskDelayUntil(&xWaitTime, T_TELEMETRY/portTICK_RATE_MS);
+    }
+}
+
+void vTelemetry(void *pvParameters)
+{
+    portTickType xTelemetryTime;
+    xTelemetryTime = xTaskGetTickCount();
+    STATE = LAUNCH;
+
+    while(1)
+    {
+        if(!telemetry_ON)
+        {
+            __delay_cycles(106);
+            vTaskResume(xWTStartHandle);
+            vTaskSuspend(NULL);
+            __delay_cycles(106);
+        }
+
+        createTelemetryPacket();
+        sciSendData(buff_size, command, 0);
+
+        PACKET_COUNT++;
+        SP1_PC++;
+        SP2_PC++;
+
+        vTaskDelayUntil(&xTelemetryTime, T_TELEMETRY);
+    }
+}
+
+void vSensors(void *pvParameters)
+{
+    portTickType xSensorsTime;
+    xSensorsTime = xTaskGetTickCount();
+
+    while(1)
+    {
+        ALTITUDE_BAR = getAltitude(PRESS_BAR);
+
+        vTaskDelayUntil(&xSensorsTime, T_SENSORS);
+    }
+}
+
+void vMissionOperations(void *pvParameters)
+{
+    portTickType xOpsTime;
+    xOpsTime = xTaskGetTickCount();
+
+    SERVO_PAYLOAD.period = 20000;
+    int angles[3] = {SPOS_ZERO, SPOS_SP1, SPOS_SP2};
+    SERVO_PAYLOAD.duty = angles[0];
+
+    int i = 0;
+    uint8 land = 0;
+    float ALTITUDE_STATES[4] = {1798.0, 1750.0, 1700.0, 1660.0};
+
+//    for(i=0 ; i<10; i++)
+//    {
+//        ALTITUDE_INIT = ALTITUDE_INIT + PRESS_BAR;
+//        vTaskDelayUntil(&xOpsTime, T_TELEMETRY);
+//    }
+//
+//    ALTITUDE_INIT = getAltitude(ALTITUDE_INIT/10);
+
+    i = 0;
+    while(1)
+    {
+        switch(STATE)
+        {
+            case SP1_RELEASE:
+                SERVO_PAYLOAD.duty = angles[1];
+                break;
+            case SP2_RELEASE:
+                SERVO_PAYLOAD.duty = angles[2];
+                break;
+            default:
+                SERVO_PAYLOAD.duty = angles[0];
+                break;
+        }
+        if(land == 0 && (ALTITUDE_BAR >= ALTITUDE_STATES[i]))
+        {
+            land = 1;
+            i++;
+            STATE = i+1;
+        }
+        if(land == 1 && (ALTITUDE_BAR <= ALTITUDE_STATES[i]))
+        {
+            i++;
+            STATE = i+1;
+        }
+        pwmSetSignal10e3(hetRAM1, PWM_PAYLOAD, SERVO_PAYLOAD);
+        vTaskDelayUntil(&xOpsTime, T_OPERATIONS);
+    }
+}
+
+void sciNotification(sciBASE_t *sci, unsigned flags )
+{
+    sciReceive(scilinREG, 1, (unsigned char *)&receivedData);
+    getCommand(receivedData[0]);
+}
+
+
+/* USER CODE END */
